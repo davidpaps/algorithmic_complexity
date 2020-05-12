b5b9cf2b1f31e73f5284b46ac8c87737dc9f7e9c
David Papamichael <davidpapamichael@gmail.com>
2020-05-12 18:46:53 +0100

    sort method refactored into own file

--- jasmine-3.5.0/src/chart.js
+++ jasmine-3.5.0/src/chart.js
@@ -1,3 +1,37 @@
-class Chart {
-  constructor() {}
+class Graph {
+  constructor() {
+    this.timingSort = new Timing(sort);
+  }
+
+  renderChart = (data, labels) => {
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "sort()",
+            data: data,
+          },
+        ],
+      },
+    });
+  };
+
+  graphData = () => {
+    this.timingSort.run(100000, 1000);
+    let labelSort = [];
+    let dataSort = [];
+
+    this.timingSort.times.forEach((iteration) => {
+      labelSort.push(iteration.inputSize);
+    });
+
+    this.timingSort.times.forEach((iteration) => {
+      dataSort.push(iteration.time);
+    });
+
+    this.renderChart(dataSort, labelSort);
+  };
 }
