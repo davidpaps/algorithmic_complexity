7a4b8558755a27351b6d7af8861a034d77251fc8
David Papamichael <davidpapamichael@gmail.com>
2020-06-08 15:40:49 +0100

    runZeroOne, runWords, runFibonacci tested in Timing

--- jasmine-3.5.0/spec/timingSpec.js
+++ jasmine-3.5.0/spec/timingSpec.js
@@ -15,4 +15,32 @@ describe("Timing", function () {
       expect(timing.times[0].time.toFixed(0)).toEqual("0");
     });
   });
+
+  describe("runZeroOne", function () {
+    it("to take in inputs and create an array with that length of 0 and 1's", function () {
+      timing.runZeroOne(1000, 100);
+      expect(timing.times.length).toEqual(10);
+      expect(timing.times[0].inputSize).toEqual(0);
+      expect(timing.times[0].time.toFixed(0)).toEqual("0");
+    });
+  });
+
+  describe("runWords", function () {
+    it("to take in inputs and create an array with that length of words", function () {
+      timing.runWords(1000, 100);
+      expect(timing.times.length).toEqual(10);
+      expect(timing.times[0].inputSize).toEqual(0);
+      expect(timing.times[0].time.toFixed(0)).toEqual("0");
+    });
+  });
+
+  describe("runFibonacci", function () {
+    it("to take in inputs and create an array with that length of words", function () {
+      let t = new Timing(fibonacci);
+      t.runFibonacci(1000, 100);
+      expect(t.times.length).toEqual(10);
+      expect(t.times[0].inputSize).toEqual(0);
+      expect(t.times[0].time.toFixed(0)).toEqual("0");
+    });
+  });
 });
